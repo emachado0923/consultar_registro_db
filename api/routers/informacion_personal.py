30efa6f3f6680e2190329ba4ec03bd1d22490739
unknown <julianusu@gmail.com>
2025-08-09 16:49:07 -0500

    autenticacion endpoints

--- api/routers/informacion_personal.py
+++ api/routers/informacion_personal.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Any, Dict
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlmodel import Session, select
 from ..core.database import get_session_analitica
@@ -6,12 +6,17 @@ from ..models.informacion_personal import (
     InformacionPersonal,
     InformacionPersonalUpdate,
 )
+from .auth import get_current_user
 
 SessionDep = Annotated[Session, Depends(get_session_analitica)]
 router = APIRouter(prefix="/informacion-personal", tags=["informacion_personal"])
 
 @router.post("/", response_model=InformacionPersonal)
-def create_informacion_personal(item: InformacionPersonal, session: SessionDep) -> InformacionPersonal:
+def create_informacion_personal(
+    item: InformacionPersonal,
+    session: SessionDep,
+    _: Dict[str, Any] = Depends(get_current_user),
+) -> InformacionPersonal:
     session.add(item)
     session.commit()
     session.refresh(item)
@@ -22,13 +27,17 @@ def read_informacion_personal(
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
+    _: Dict[str, Any] = Depends(get_current_user),
 ) -> list[InformacionPersonal]:
     items = session.exec(select(InformacionPersonal).offset(offset).limit(limit)).all()
     return items
 
 @router.patch("/{docconvfondo}", response_model=InformacionPersonal)
 def update_informacion_personal(
-    docconvfondo: str, data: InformacionPersonalUpdate, session: SessionDep
+    docconvfondo: str,
+    data: InformacionPersonalUpdate,
+    session: SessionDep,
+    _: Dict[str, Any] = Depends(get_current_user),
 ) -> InformacionPersonal:
     item = session.get(InformacionPersonal, docconvfondo)
     if not item:
@@ -42,7 +51,11 @@ def update_informacion_personal(
     return item
 
 @router.delete("/{docconvfondo}")
-def delete_informacion_personal(docconvfondo: str, session: SessionDep):
+def delete_informacion_personal(
+    docconvfondo: str,
+    session: SessionDep,
+    _: Dict[str, Any] = Depends(get_current_user),
+):
     item = session.get(InformacionPersonal, docconvfondo)
     if not item:
         raise HTTPException(status_code=404, detail="Registro no encontrado")
